72b7f060066dd6518967a6bb7a4da61d1e538d86
insertdead <mdsc0111@tuta.io>
2022-01-05 16:06:57 -0500

    gort

--- main.py
+++ main.py
@@ -1,76 +1,60 @@
-import pygame
 import random
 
+import pygame
+import pygame.draw.circle as circ
 
-#default: (255, 255, 255)
-background_colour = (232, 224, 209)
+# default: (255, 255, 255)
+BG_COLOUR = (232, 224, 209)
 
-#default: (52, 137, 227)
-line_color = (100, 100, 100)
+# default: (52, 137, 227)
+LINE_COLOUR = (100, 100, 100)
 
-#default: (0, 0, 0)
-axis_color = (0, 0, 0)
+# default: (0, 0, 0)
+AXIS_COLOUR = (0, 0, 0)
 
-(width, height) = (800, 400)
+WIDTH,HEIGHT) = (800, 400)
 
-screen = pygame.display.set_mode((width, height))
+SCREEN = pygame.display.set_mode(WIDTH,HEIGHT))
 
-screen.fill(background_colour)
+screen.fill(BG_COLOUR)
 
 icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
 
 
-def circ(surface, color, pos, dia):
-
-    pygame.draw.circle(surface, color, pos, dia)
-
-
 def graph(x, b, xvalue):
-
     equation = ""
-
     xvalue = xvalue * -1
 
     if x != 0:
-
         equation = equation + str(x) + " * "
 
     equation = equation + str(xvalue) + " ** 2"
 
     if b != 0:
-
         equation = equation + " + " + str(b)
 
-    return(equation)
+    return equation
 
 
 def windowtext(x, h, b):
-
     equation = ""
-
     equation = "y = "
 
     if x != 0:
-
         equation = equation + str(x)
 
     if h != 0:
-
         if h <= 0:
-
             equation = equation + "(x + " + str(abs(h)) + ")²"
 
         elif h >= 0:
-
             equation = equation + "(x - " + str(abs(h)) + ")²"
 
     if h == 0:
-
         equation = equation + "x²"
 
     if b != 0:
-
         equation = equation + " + " + str(abs(b))
 
     equation = equation + " | PotatoCouch's graph visualiser"
@@ -79,11 +63,11 @@ def windowtext(x, h, b):
 
 def displaygraph(x, h, b, inbetween):
 
-    hwidth = width / 2
-    hheight = height / 2
+    hwidth = WIDTH / 2
+    hheight = HEIGHT / 2
     inbetween = inbetween * -1
-    count = width * -1
-    ng_h = height * -1
+    count = WIDTH * -1
+    ng_h = HEIGHT * -1
 
     if x == "random":
         x = random.uniform(0.00000001, 100)
@@ -96,20 +80,18 @@ def displaygraph(x, h, b, inbetween):
 
     pygame.display.set_caption(windowtext(x, h, b))
 
-    while count <= width + 1:
+    while count <= WIDTH + 1:
 
-        if abs(eval(graph(x, b, count))) <= height:
+        if abs(eval(graph(x, b, count))) <= HEIGHT:
 
             if count < 0:
 
-                pos = (count + hwidth + h,
-                       ((eval(graph(x, b, count))) * -1) + hheight)
+                pos = (count + hwidth + h, ((eval(graph(x, b, count))) * -1) + hheight)
                 circ(screen, line_color, pos, 1)
 
             else:
 
-                pos = (count + hwidth + h,
-                       eval(graph(x, (b * -1), count)) + hheight)
+                pos = (count + hwidth + h, eval(graph(x, (b * -1), count)) + hheight)
                 circ(screen, line_color, pos, 1)
 
         pygame.display.flip()
@@ -117,21 +99,21 @@ def displaygraph(x, h, b, inbetween):
         count += 1 * 10 ** inbetween
 
 
-def drawlines(axiscolor):
+def drawlines(AXIS_COLOUR):
 
-    count = width * -1
+    count = WIDTH * -1
 
-    hwidth = width / 2
-    hheight = height / 2
+    hwidth = WIDTH / 2
+    hheight = HEIGHT / 2
 
-    while count <= width + 1:
+    while count <= WIDTH + 1:
 
-        circ(screen, axiscolor, (hwidth, count), 1)
-        circ(screen, axiscolor, (count, hheight), 1)
+        circ(screen, AXIS_COLOUR, (hwidth, count), 1)
+        circ(screen, AXIS_COLOUR, (count, hheight), 1)
 
         if count % 20 == 0:
-            circ(screen, axiscolor, (hwidth, count), 3)
-            circ(screen, axiscolor, (count, hheight), 3)
+            circ(screen, AXIS_COLOUR, (hwidth, count), 3)
+            circ(screen, AXIS_COLOUR, (count, hheight), 3)
         pygame.display.flip()
         count += 1
 
@@ -144,10 +126,10 @@ while running:
 
     if do_once is True:
 
-        drawaxes = width * -1
+        drawaxes = WIDTH * -1
         pygame.display.flip()
         drawlines(axis_color)
-        displaygraph(20/10000, 10, 10, 1)
+        displaygraph(20 / 10000, 10, 10, 1)
 
         do_once = False
 
